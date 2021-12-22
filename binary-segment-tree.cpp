97bf2a1b6a3020a8e1b5aed5fe229b48ffc41248
Mahmudul-Kabir <59996575+Mahmudul-Kabir@users.noreply.github.com>
2021-12-22 17:05:41 +0600

    Update binary-segment-tree.cpp

--- binary-segment-tree.cpp
+++ binary-segment-tree.cpp
@@ -23,11 +23,11 @@ struct bin_str{
         str[node] = fun(str[left],str[left + 1]);
         return;
     }
-    ll que(ll a,ll b,ll l,ll r,ll node){
+    ll query(ll a,ll b,ll l,ll r,ll node){
         if(b < l || a > r) return def;
         if(a >= l && b <= r) return str[node];
         ll left = node * 2, mid = a + (b - a)/2;
-        return fun(que(a,mid,l,r,left),que(mid+1,b,l,r,left+1));
+        return fun(query(a,mid,l,r,left),query(mid+1,b,l,r,left+1));
     }
     bin_str(ll x,vector<ll > &v) : id(v){
         str.assign(x * 4,def);
